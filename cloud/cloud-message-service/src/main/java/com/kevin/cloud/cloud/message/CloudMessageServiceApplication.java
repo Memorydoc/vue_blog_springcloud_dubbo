0155d8daf023e0fb696001a59be528d45b3c7386
司泽刚 <1322287292@qq.com>
2020-01-10 16:53:36 +0800

    #

---
+++ cloud/cloud-message-service/src/main/java/com/kevin/cloud/cloud/message/CloudMessageServiceApplication.java
@@ -0,0 +1,10 @@
+package com.kevin.cloud.cloud.message;
+
+/**
+ * @program: kevin-cloud-dubbo2.0
+ * @description: 日志服务
+ * @author: kevin
+ * @create: 2020-01-10 16:50
+ **/
+public class CloudMessageServiceApplication {
+}
