c3954213676b6ea2e6df529a9042cc220c4328d8
司泽刚 <1322287292@qq.com>
2020-01-10 14:31:52 +0800

    sentinle熔断降级

--- configuration/configuration-sentinel/src/main/java/com/kevin/cloud/config/DubboSentinelConfiguration.java
+++ configuration/configuration-sentinel/src/main/java/com/kevin/cloud/config/DubboSentinelConfiguration.java
@@ -9,11 +9,6 @@ import org.springframework.context.annotation.Configuration;
  * <p>
  * Description: 仅 Dubbo 服务需要该配置，Spring Cloud Alibaba 无需加载该配置
  * </p>
- *
- * @author Lusifer
- * @version v1.0.0
- * @date 2019-08-31 03:00:47
- * @see com.funtl.myshop.plus.configuration
  */
 @Configuration
 public class DubboSentinelConfiguration {
